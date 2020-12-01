0d502cff069f053b30e13097480ed413aafe43c6
Marius Kilian <marius@Mariuss-MacBook-Pro.local>
2020-12-01 13:57:14 +0100

    Finished P1

--- Day_1/P1_Report_Repair.py
+++ Day_1/P1_Report_Repair.py
@@ -1,1 +1,7 @@
-print("Hello World")
+txt = open("P1_input.txt").read().split()
+
+for i in range(len(txt)):
+    for j in range(i):
+        x = int(txt[i])
+        y = int(txt[j])
+        if x + y == 2020: print(x * y)
