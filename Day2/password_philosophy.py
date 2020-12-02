43a02298e82edfbc4b5d8d0938da99836f0c9b53
Marius Kilian <marius@Mariuss-MacBook-Pro.local>
2020-12-02 15:40:34 +0100

    Comments

--- Day2/password_philosophy.py
+++ Day2/password_philosophy.py
@@ -4,7 +4,7 @@ txt = open("input.txt").read().splitlines()
 num_valid_pass = 0
 for line in txt:
     parts = line.split()
-    lim = list(map(int, parts[0].split('-')))
+    lim = [int(n) for n in parts[0].split('-')]
     ltr = parts[1].strip(':')
     wrd = parts[2]
     if lim[0] <= wrd.count(ltr) <= lim[1]:
@@ -15,9 +15,23 @@ print(num_valid_pass)
 num_valid_pass = 0
 for line in txt:
     parts = line.split()
-    lim = list(map(int, parts[0].split('-')))
+    lim = [int(n) for n in parts[0].split('-')]
     ltr = parts[1].strip(':')
     wrd = parts[2]
     if (wrd[lim[0]-1] == ltr) ^ (wrd[lim[1]-1] == ltr):
         num_valid_pass += 1   
 print(num_valid_pass)
+
+# Alternatives:
+
+#     cmin = line.split('-')[0]
+#     cmax = line.split('-')[1].split()[0]
+#     lttr = line.split('-')[1].split()[1].strip(':')
+#     word = line.split('-')[1].split()[2]
+
+#     line1 = line.split('-')
+#     line2 = line1[1].split()
+#     cmin = line1[0]
+#     cmax = line2[0]
+#     lttr = line2[1].strip(':')
+#     word = line2[2]
